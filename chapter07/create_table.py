5955d8c9089b4dd176761a68a8d0c90dce4b406b
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-02-10 20:32:32 +0900

    Create create_table.py

---
+++ chapter07/create_table.py
@@ -0,0 +1,32 @@
+import pymysql
+
+db = None
+try:
+    db = pymysql.connect(
+        host='127.0.0.1',
+        user='homestead',
+        passwd='secret',
+        db='homestead',
+        charset='utf8'
+    )
+
+    # 테이블 생성 sql정의
+    sql = '''
+    CREATE TABLE tb_student (
+        id int primary key auto_increment not null,
+        name varchar(32),
+        age int,
+        address varchar(32)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+    '''
+
+    # 테이블 생성
+    with db.cursor() as cursor:
+        cursor.execute(sql)
+
+except Exception as e:
+    print(e)
+
+finally:
+    if db is not None:
+        db.close()
