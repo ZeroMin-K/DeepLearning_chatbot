3a83defd2c0ecc80c1a57716c6c11d0582a35980
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-02-10 20:46:31 +0900

    Create insert_data.py

---
+++ chapter07/insert_data.py
@@ -0,0 +1,27 @@
+import pymysql
+
+db = None
+try:
+    db = pymysql.connect(
+        host='127.0.0.1',
+        user='homestead',
+        passwd='secret',
+        db='homestead',
+        charset='utf8'
+    )
+
+    # 데이터 삽입 sql 정의
+    sql = '''
+        INSERT  tb_student(name, age, address) values('Kei', 35, 'Korea')
+    '''
+
+    # 데이터 삽입
+    with db.cursor() as cursor:
+        cursor.execute(sql)
+    db.commit()
+
+except Exception as e:
+    print(e)
+finally:
+    if db is not None:
+        db.close()
